4b4697322fe634d24976be262afe4029015a7d96
Adetimmy <ogunleademola@gmail.com>
2025-07-12 14:42:04 +0100

    creating an api to fetch products

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios, { AxiosError } from "axios";
 
 const getProducts = async () => {
-    try {
-        const res = await axios.get('/products.json')
-    } catch (error) {
-        
+  try {
+    const res = await axios.get("/products.json");
+    if (res.statusText === "OK") {
+      return { data: res.data, error: null };
     }
-} 
+    return { data: null, error: res.data };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      return { error: error.message ?? error.response?.data, data: null };
+    }
+    return { error: "Something went wrong, try again later", data: null };
+  }
+};
