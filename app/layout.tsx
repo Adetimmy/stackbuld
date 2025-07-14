2bb5c4e6a63707910fd325cc18380d3ec7850def
Adetimmy <ogunleademola@gmail.com>
2025-07-14 03:10:21 +0100

    refactor: changing fonts to poppins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,21 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
 import './globals.css';
 import ReactQueryProvider from '@/lib/provider';
 import Header from '@/components/header';
 import Footer from '@/components/footer';
+import { Poppins } from 'next/font/google';
 import { PageMetaData } from '@/seo-config';
 
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: [ '300', '400', '500', '600', '700'],
+  style: 'normal',
 });
 
+
 const meta = PageMetaData.Home;
+
 export const metadata: Metadata = {
   ...meta,
 };
@@ -30,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-full min-h-dvh relative`}
+        className={`${poppins.className} antialiased w-full min-h-dvh relative`}
       >
         <ReactQueryProvider>
           <Header />
